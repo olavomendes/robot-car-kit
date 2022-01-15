0a889baee0aa5780c66de797c67a3fd2772a0205
Olavo <olavo.77@hotmail.com>
2022-01-15 17:16:02 -0300

    fix: corrigido nome da pasta, do arquivo e nomes das variaveis para melhor entendimento do codigo

---
+++ infraredSensorTest/infraredSensorTest.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>
+
+int InfraredReceiverPin = 12;
+ 
+IRrecv irrecv(InfraredReceiverPin);
+decode_results results;
+
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX); 
+    irrecv.resume(); // Recebe o próximo valor
+  }
+}
