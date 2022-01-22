becd1d15e44d658dc991882233a1804b02808db9
Olavo <olavo.77@hotmail.com>
2022-01-22 16:55:15 -0300

    fix: removidos comentarios desnecessarios

--- L298NMotorTest/L298NMotorTest.ino
+++ L298NMotorTest/L298NMotorTest.ino
@@ -1,4 +1,3 @@
-// Entradas do L298N
 int IN1 = 2;
 int IN2 = 4;
 int IN3 = 7;
@@ -14,40 +13,22 @@ void setup() {
 }
 
 void loop() {
-  // Velocidade das rodas direitas
   analogWrite(ENA, 200);
-  // Velocidade das rodas esquerdas
   analogWrite(ENB, 200);
 
-  // Vai pra frente
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
   delay(2000);
-
-  // Para
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
-  delay(2000);
-
-  // Para - código alternativo
-  // analogWrite(ENA, 0);
-  // analogWrite(ENB, 0);
   
-  // Volta
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
   delay(2000);
 
-  // Para novamente
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
   delay(2000);
 }
